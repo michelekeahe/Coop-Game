c47397379505b12370abbab9d035673bc7c09d3a
sinthos11 <89173577+sinthos11@users.noreply.github.com>
2021-08-21 20:59:48 -1000

    More stuff

--- Co-op Game/Assets/Scripts/Enemy/EnemyMeleCombat.cs
+++ Co-op Game/Assets/Scripts/Enemy/EnemyMeleCombat.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class EnemyMeleCombat : MonoBehaviour
 {
-    [SerializeField]
-    private float minMeleTime = .5f;
-    [SerializeField]
-    private float maxMeleTime = 1f;
+
 
     private void Update()
     {
-        
+
     }
+
+    
 
 
 
--- Co-op Game/Assets/Scripts/Player/PlayerHealth.cs
+++ Co-op Game/Assets/Scripts/Player/PlayerHealth.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     private int health = 3;
-    EnemyMeleCombat meleAttackDelay;
 
     private void Start()
     {
-        meleAttackDelay = new EnemyMeleCombat();
     }
 
-
+    //When player runs into enemy, player becomes immune to all colliders and loses 1 health.
+    //Immunity is lost after x seconds
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "MeleEnemy")
         {
+            this.gameObject.layer = LayerMask.NameToLayer("IgnoreCollisions");
             health = health- 1;
+            Invoke(nameof(TurnOnCollisions), 3f);
             Debug.Log(health);
         }
 
+
+    }
+
+    private void TurnOnCollisions()
+    {
+        this.gameObject.layer = LayerMask.NameToLayer("Player");
     }
     
 
